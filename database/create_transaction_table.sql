d8a3a76aa5693f5130dc9aa1208d44d9c6c7a06a
Julian Steiner <julian.steiner@autoelectric.com>
2020-04-09 07:49:47 +0200

    Create table script for transaction table.

---
+++ database/create_transaction_table.sql
@@ -0,0 +1,24 @@
+-- Table: public.bc_transaction
+
+-- DROP TABLE public.bc_transaction;
+
+CREATE TABLE public.bc_transaction
+(
+    bct_id uuid NOT NULL,
+    bct_block_id uuid NOT NULL,
+    bct_transmitter_id uuid NOT NULL,
+    bct_receiver_id uuid NOT NULL,
+    bct_creditvalue bigint NOT NULL,
+    bct_hash text COLLATE pg_catalog."default",
+    CONSTRAINT bc_transaction_pkey PRIMARY KEY (bct_id),
+    CONSTRAINT bc_block_fkey FOREIGN KEY (bct_block_id) REFERENCES public.bc_block (bcb_id),
+    CONSTRAINT bc_transmitter_fkey FOREIGN KEY (bct_transmitter_id) REFERENCES public.bc_participant (bcp_id),
+    CONSTRAINT bc_receiver_fkey FOREIGN KEY (bct_receiver_id) REFERENCES public.bc_participant (bcp_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.bc_transaction
+    OWNER to blockchain_admin;
+COMMENT ON TABLE public.bc_transaction
+    IS 'Transaction table for blockchain network';
