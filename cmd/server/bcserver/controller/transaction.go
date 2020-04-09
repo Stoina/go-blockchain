a8729a420c0e94ff88fe6ed7957a1f43ad7d5456
Julian Steiner <julian.steiner@autoelectric.com>
2020-04-09 14:26:36 +0200

    Integrate controller and repository interface.

--- cmd/server/bcserver/controller/transaction.go
+++ cmd/server/bcserver/controller/transaction.go
@@ -1,1 +1,85 @@
 package controller
+
+import (
+	"net/http"
+	"net/url"
+
+	db "github.com/stoina/go-database"
+
+	repo "github.com/stoina/go-blockchain/cmd/server/bcserver/repository"
+	result "github.com/stoina/go-blockchain/cmd/server/bcserver/repository/result"
+)
+
+// TransactionController exported
+// ...
+type TransactionController struct {
+	url        string
+	name       string
+	repository repo.Repository
+	dbConn     *db.Connection
+}
+
+// NewTransactionController exorted
+// ...
+func NewTransactionController(dbConn *db.Connection) *TransactionController {
+	return &TransactionController{
+		name:       "Transaction Controller",
+		url:        "transaction",
+		repository: repo.NewTransactionRepository(),
+		dbConn:     dbConn}
+}
+
+// URL exported
+// ...
+func (tc *TransactionController) URL() string {
+	return tc.url
+}
+
+// Name exported
+// ...
+func (tc *TransactionController) Name() string {
+	return tc.name
+}
+
+// Repository exported
+// ...
+func (tc *TransactionController) Repository() repo.Repository {
+	return tc.repository
+}
+
+func (tc *TransactionController) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	HandleRequest(writer, request, tc)
+}
+
+// Post exported
+// ...
+func (tc *TransactionController) Post(contentType string, content string) *result.RepositoryResult {
+	return nil
+}
+
+// Put exported
+// ...
+func (tc *TransactionController) Put(par string) *result.RepositoryResult {
+	return nil
+
+}
+
+// Patch exported
+// ...
+func (tc *TransactionController) Patch(par string) *result.RepositoryResult {
+	return nil
+
+}
+
+// Delete exported
+// ...
+func (tc *TransactionController) Delete(par string) *result.RepositoryResult {
+	return nil
+
+}
+
+// Get exported
+// ...
+func (tc *TransactionController) Get(calledURL *url.URL) *result.RepositoryResult {
+	return nil
+}
--- cmd/server/bcserver/repository/transaction.go
+++ cmd/server/bcserver/repository/transaction.go
@@ -1,6 +1,53 @@
 package repository
 
+import (
+	"github.com/stoina/go-blockchain/cmd/server/bcserver/repository/result"
+	db "github.com/stoina/go-database"
+)
+
 // TransactionRepository exported
 // ...
 type TransactionRepository struct {
+}
+
+// NewTransactionRepository exported
+// ...
+func NewTransactionRepository() *TransactionRepository {
+	return &TransactionRepository{}
+}
+
+// Create exported
+// ...
+func (tp *TransactionRepository) Create(jsonString string, db *db.Connection) *result.RepositoryResult {
+	return nil
+}
+
+// Update exported
+// ...
+func (tp *TransactionRepository) Update(jsonString string, db *db.Connection) *result.RepositoryResult {
+	return nil
+}
+
+// Delete exported
+// ...
+func (tp *TransactionRepository) Delete(jsonString string, db *db.Connection) *result.RepositoryResult {
+	return nil
+}
+
+// ReadAll exported
+// ...
+func (tp *TransactionRepository) ReadAll(db *db.Connection) *result.RepositoryResult {
+	return nil
+}
+
+// ReadByID exported
+// ...
+func (tp *TransactionRepository) ReadByID(id string, db *db.Connection) *result.RepositoryResult {
+	return nil
+}
+
+// ReadByProperty exported
+// ...
+func (tp *TransactionRepository) ReadByProperty(propertyName string, value interface{}, db *db.Connection) *result.RepositoryResult {
+	return nil
 }
