35852ac8dc6c674bec5f982169af0fc9011ba95d
Julian Steiner <julian.steiner@autoelectric.com>
2020-04-09 15:24:57 +0200

    Change int to numeric datatype.

--- database/create_transaction_table.sql
+++ database/create_transaction_table.sql
@@ -5,10 +5,10 @@
 CREATE TABLE public.bc_transaction
 (
     bct_id uuid NOT NULL,
-    bct_block_id uuid NOT NULL,
+    bct_block_id uuid NULL,
     bct_transmitter_id uuid NOT NULL,
     bct_receiver_id uuid NOT NULL,
-    bct_creditvalue bigint NOT NULL,
+    bct_creditvalue numeric NOT NULL,
     bct_hash text COLLATE pg_catalog."default",
     CONSTRAINT bc_transaction_pkey PRIMARY KEY (bct_id),
     CONSTRAINT bc_block_fkey FOREIGN KEY (bct_block_id) REFERENCES public.bc_block (bcb_id),
